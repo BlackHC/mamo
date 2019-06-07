bb71aa67ffedd82fbcc55e39ac8768d2dcd1738b
Andreas Kirsch <blackhc@gmail.com>
2019-06-07 15:05:27 +0100

    And a small test and it "works"

--- dumbo/__init__.py
+++ dumbo/__init__.py
@@ -2,13 +2,25 @@ from dumbo.internal.stopwatch_context import StopwatchContext
 from dataclasses import dataclass
 
 
+def estimate_size(value):
+    return 4
+
+
+def estimate_throughput(value):
+    return 2**30
+
+
+def estimate_threshold(value):
+    return estimate_size(value)/estimate_throughput(value)
+
+
 def dumbo():
     def wrapper(func):
         cache = {}
 
         def wrapped_func(*args, **kwargs):
             # Check whether the result has already been cached?
-            key = (args, list(kwargs.items()))
+            key = (args, tuple(kwargs.items()))
             if key in cache:
                 return cache[key]
             with StopwatchContext() as stopwatch_context:
---
+++ tests/test_dumbo.py
@@ -0,0 +1,21 @@
+import dumbo
+import time
+import dumbo.internal.stopwatch_context as swc
+
+
+@dumbo.dumbo()
+def slow_operation(a:int, b:int):
+    time.sleep(a)
+    return a + b
+
+
+def test_slow_operation():
+    with swc.StopwatchContext() as first_run:
+        result = slow_operation(1,3)
+    assert result == 1 + 3
+
+    with swc.StopwatchContext() as cached_run:
+        result_cached = slow_operation(1, 3)
+
+    assert result_cached == result
+    assert cached_run.elapsed_time < first_run.elapsed_time
